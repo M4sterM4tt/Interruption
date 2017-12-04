94943187bc018c583b6759a3e9f78848f24b951a
M4sterM4tt <WAIM1_16@uni.worc.ac.uk>
2017-12-04 11:30:03 +0000

    Madder

--- www/js/index.js
+++ www/js/index.js
@@ -34,16 +34,21 @@ function createMessage(){
     var currentTime = new Date().getTime(); //current time
     var notificationTime = new Date(currentTime + 3000); // delayed time  - add 1 second   
     
-    cordova.plugins.notification.local.schedule({
-        title: 'The Mad Man',
-        text: 'You wanna Fight',
+    cordova.plugins.notification.local.schedule([
+    {
+        id: 		1,
+        title:      'The Mad Man',
+        text:       'You wanna Fight',
         date: 		notificationTime, 
         badge: 		notification_count++,
-        actions: [
-            { id: 'yes', title: 'Yes' },
-            { id: 'no',  title: 'No' }
-        ]
-    });
+    },
+    {
+        id: 		2,
+        title:      'The Madder Man',
+        text:       'You wanna Fight',
+        date: 		notificationTime, 
+        badge: 		notification_count++,
+    }]);
 }
         	
 
