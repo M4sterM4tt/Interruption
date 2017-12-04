f55ac2ad4d3d363a5d10559043019ab553a2e4d5
M4sterM4tt <WAIM1_16@uni.worc.ac.uk>
2017-12-04 11:33:41 +0000

    stupid hydra?

--- www/js/index.js
+++ www/js/index.js
@@ -24,7 +24,25 @@ function createMessage(){
     
     new Toast({content: 'Stop!!!   You Mad Man!!!', duration: 2000}); 	
     // new Toast({content: 'Never Mind, I was a Fool', duration: 1000}); 
-
+    
+    var currentTime = new Date().getTime(); //current time
+    var notificationTime = new Date(currentTime + 3000); // delayed time  - add 1 second   
+    
+    cordova.plugins.notification.local.schedule([
+    {
+        id: 		1,
+        title:      'The Mad Man',
+        text:       'You wanna Fight',
+        date: 		notificationTime, 
+        badge: 		notification_count++,
+    },
+    {
+        id: 		2,
+        title:      'The Madder Man',
+        text:       'You wanna Fight',
+        date: 		notificationTime, 
+        badge: 		notification_count++,
+    }]);
 }
         	
 
