de034ca41797ffba5bad849de3d7d383bc1a32a8
M4sterM4tt <WAIM1_16@uni.worc.ac.uk>
2017-12-04 11:58:51 +0000

    on click

--- www/js/index.js
+++ www/js/index.js
@@ -34,9 +34,8 @@ function createMessage(){
         title:      'The Mad Man',
         text:       'You wanna Fight',
         date: 		notificationTime, 
-        badge: 		notification_count++,
-        createDialog()
-    },
+        badge: 		notification_count++
+    },      
     {
         id: 2,
         title: 'Chat with Mad Man',
@@ -64,20 +63,25 @@ function createDialog() {
 }         	
 function dialogDismissed(buttonIndex) {
 	
+    
 	if (buttonIndex==1) {
         new Toast({content: "You know what? No.", duration: 3000});
  
         var currentTime = new Date().getTime(); //current time
         var notificationTime = new Date(currentTime + 3000); // delayed time  - add 1 second
-    			
+    	
         cordova.plugins.notification.local.schedule({ 
     	    id: 		3,
             title: 		"Actually, wanna get some Chips",
-            message: 	"Here is your Food, You better pay me back",
+            message: 	"Hey, are you hungry?",
             date: 		notificationTime, 
             badge: 		notification_count++
    	    });
+        
+        cordova.plugins.notification.local.on("click", function createDialog())    
     }
+      
+            
    	else if(buttonIndex==2) {
         new Toast({content: 'Fine, but you must pay me back', duration: 3000});
         
@@ -91,8 +95,7 @@ function dialogDismissed(buttonIndex) {
             date: 		notificationTime, 
             badge: 		notification_count++
    	    });
-    }
-    
+    }    
 }
 
   
