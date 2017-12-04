62e1f8f08c56f59d1209487be03d55909277d4ef
M4sterM4tt <WAIM1_16@uni.worc.ac.uk>
2017-12-04 11:03:18 +0000

    Food?

--- www/js/index.js
+++ www/js/index.js
@@ -21,6 +21,7 @@ $(document).on('pageinit', function() {
 });
 
 
+
 function createMessage(){
     
 	// phoneGap and jQueryMobile do not support toast messages directly.
@@ -30,6 +31,7 @@ function createMessage(){
     // new Toast({content: 'Never Mind, I was a Fool', duration: 1000}); 
 }
         	
+
 
 function createDialog() {
 
@@ -43,17 +45,31 @@ function createDialog() {
         ['No, But Give Me Some Food Anyway', 'Yes, Give Me Food']  // buttons
     );
 
-}
-        	
-           	
+}         	
 function dialogDismissed(buttonIndex) {
 	
-	if(buttonIndex==1) new Toast({content: "You know what? No.", duration: 3000});
-   	else if(buttonIndex==2) new Toast({content: 'Fine, but you must pay me back', duration: 3000});
-
+	if (buttonIndex==1) {
+        new Toast({content: "You know what? No.", duration: 3000});
+    }
+   	else if(buttonIndex==2) {
+        new Toast({content: 'Fine, but you must pay me back', duration: 3000});
+        
+        var currentTime = new Date().getTime(); //current time
+        var notificationTime = new Date(currentTime + 3000); // delayed time  - add 1 second
+    			
+        cordova.plugins.notification.local.schedule({ 
+    	   id: 		1,
+            title: 		"Food",
+            message: 	"Here is your Food, You better pay me back",
+            date: 		notificationTime, 
+            badge: 		notification_count++
+   	    });
+    }
+    
 }
 
-     
+  
+
 function createNotification() {
         		
     //generate a time to post notification
@@ -66,7 +82,7 @@ function createNotification() {
     cordova.plugins.notification.local.schedule({ 
     	id: 		1,
         title: 		"Hey you",
-        message: 	"This is an example notification",
+        message: 	"It's Ya Boy!",
         date: 		notificationTime, 
         badge: 		notification_count++
    	});
